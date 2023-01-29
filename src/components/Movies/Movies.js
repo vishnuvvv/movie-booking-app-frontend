a12cd7667b3ba2d94bfbc51f76f59d33524c349c
VISHNU V <vishnusathyanathan@gmail.com>
2023-01-30 01:23:09 +0530

    created movies page

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,7 +1,50 @@
-import React from "react";
+import { Box, Typography } from "@mui/material";
+import React, { useEffect, useState } from "react";
+import { getAllMovies } from "../../api-helpers/api-helpers";
+import MovieItem from "./MovieItem";
 
 const Movies = () => {
-  return <div>Movies</div>;
+  const [movies, setMovies] = useState([]);
+  useEffect(() => {
+    getAllMovies()
+      .then((data) => setMovies(data.movies))
+      .catch((err) => console.log(err));
+  }, []);
+
+  return (
+    <Box margin={"auto"} marginTop={4}>
+      <Typography
+        margin={"auto"}
+        variant="h4"
+        padding={2}
+        width="40%"
+        bgcolor={"#900C3F"}
+        color="white"
+        textAlign={"center"}
+      >
+        All Movies
+      </Typography>
+      <Box
+        width="100%"
+        margin="auto"
+        marginTop={5}
+        display="flex"
+        justifyContent="flex-start"
+        flexWrap="wrap"
+      >
+        {movies &&
+          movies.map((movie, index) => (
+            <MovieItem
+              key={index}
+              id={movie.id}
+              posterUrl={movie.posterUrl}
+              title={movie.title}
+              realeaseDate={movie.realeaseDate}
+            />
+          ))}
+      </Box>
+    </Box>
+  );
 };
 
 export default Movies;

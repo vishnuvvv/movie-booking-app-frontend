38d7bf56d9a4418e38f866614b481849c53723fc
VISHNU V <vishnusathyanathan@gmail.com>
2023-02-10 00:00:14 +0530

    sdfghj

--- src/App.js
+++ src/App.js
@@ -1,28 +1,30 @@
-import React from 'react'
-import { Route, Routes } from 'react-router-dom'
-import Header from './components/Header'
-import HomePage from './components/HomePage.js'
-import Admin from './components/Admin/Admin.js'
-import Auth from './components/Auth/Auth.js'
-import Movies from './components/Movies/Movies.js'
-
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage.js";
+import Admin from "./components/Admin/Admin.js";
+import Auth from "./components/Auth/Auth.js";
+import Movies from "./components/Movies/Movies.js";
+import { useSelector } from "react-redux";
 
 const App = () => {
+  const isAdminLoggedIn = useSelector((state) => state.admin.isLoggedIn);
+  const isUserLoggedIn = useSelector((state)=>state.user.isLoggedIn)
+  console.log(isAdminLoggedIn);
+  console.log(isUserLoggedIn);
   return (
     <>
-      <Header/>
+      <Header />
       <section>
         <Routes>
-          <Route path='/' element={<HomePage/>} />
-          <Route path="/movies" element={<Movies/>} />
-          <Route  path='/admin' element={<Admin/>}/>
-          <Route path='/auth' element={<Auth/>}/>
-    
+          <Route path="/" element={<HomePage />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/admin" element={<Admin />} />
+          <Route path="/auth" element={<Auth />} />
         </Routes>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;

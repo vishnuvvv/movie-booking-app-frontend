19fd10e9d0d9b70eacc9d51be1a5dd2d04202bb3
VISHNU V <vishnusathyanathan@gmail.com>
2023-02-11 01:00:34 +0530

    completed auth

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
 import HomePage from "./components/HomePage.js";
 import Admin from "./components/Admin/Admin.js";
 import Auth from "./components/Auth/Auth.js";
 import Movies from "./components/Movies/Movies.js";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { adminActions, userActions } from "./store";
 
 const App = () => {
+  const dispatch = useDispatch();
   const isAdminLoggedIn = useSelector((state) => state.admin.isLoggedIn);
-  const isUserLoggedIn = useSelector((state)=>state.user.isLoggedIn)
-  console.log(isAdminLoggedIn);
-  console.log(isUserLoggedIn);
+  const isUserLoggedIn = useSelector((state) => state.user.isLoggedIn);
+  console.log("isAdminLoggedIn:", isAdminLoggedIn);
+  console.log("isUserLoggedIn:", isUserLoggedIn);
+  useEffect(() => {
+    if (localStorage.getItem("userId")) {
+      dispatch(userActions.login());
+    } else if (localStorage.getItem("adminId")) {
+      dispatch(adminActions.login());
+    }
+  }, []);
   return (
     <>
       <Header />

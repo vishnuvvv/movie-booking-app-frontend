762a837be2b5738d52448bdef8cb74ee25478681
VISHNU V <vishnusathyanathan@gmail.com>
2023-02-15 23:52:29 +0530

    booking form completed

--- src/components/Bookings/Booking.js
+++ src/components/Bookings/Booking.js
@@ -1,4 +1,4 @@
-import { Box, FormLabel, TextField, Typography } from "@mui/material";
+import { Box, Button, FormLabel, TextField, Typography } from "@mui/material";
 import React, { Fragment, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getMovieDetails } from "../../api-helpers/api-helpers";
@@ -27,7 +27,7 @@ const Booking = () => {
           >
             Book Tickets Of Movie : {movie.title}
           </Typography>
-          <Box display={"flex"} justifyContent={"center"} >
+          <Box display={"flex"} justifyContent={"center"}>
             <Box
               display={"flex"}
               flexDirection="cloumn"
@@ -45,25 +45,41 @@ const Booking = () => {
               <Box width={"80%"} marginTop={3} padding={2}>
                 <Typography paddingTop={2}>{movie.description}</Typography>
                 <Typography fontWeight={"bold"} marginTop={1}>
-                  Starrer:{movie.actors.map((actor) =>" "+ actor + ", ")}
+                  Starrer:{movie.actors.map((actor) => " " + actor + ", ")}
                 </Typography>
                 <Typography fontWeight={"bold"} marginTop={1}>
-                  Release Date : { new Date( movie.releaseDate).toDateString()}
+                  Release Date : {new Date(movie.releaseDate).toDateString()}
                 </Typography>
               </Box>
+            </Box>
 
-              <Box width={"50%"} paddingTop={3}>
-                <form>
-                  <Box padding={5}
+            <Box width={"50%"} paddingTop={3}>
+              <form>
+                <Box
+                  padding={5}
                   margin={"auto"}
                   display={"flex"}
                   flexDirection={"column"}
-                  >
-                    <FormLabel>Seat Number</FormLabel>
-                    <TextField name="seatNumber"  type={"number"} margin="normal" />
-                  </Box>
-                </form>
-              </Box>
+                >
+                  <FormLabel>Seat Number</FormLabel>
+                  <TextField
+                    name="seatNumber"
+                    type={"number"}
+                    margin="normal"
+                    variant="standard"
+                  />
+                  <FormLabel>Booking Date</FormLabel>
+                  <TextField
+                    name="date"
+                    type={"date"}
+                    margin="normal"
+                    variant="standard"
+                  />
+                  <Button type="submit" sx={{ mt: 3 }}>
+                    Book now
+                  </Button>
+                </Box>
+              </form>
             </Box>
           </Box>
         </Fragment>

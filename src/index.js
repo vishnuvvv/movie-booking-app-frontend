592443ed8daf2bb9945af9dc25439f92b91bccd7
vishnu <vishnusathyanathan@gmail.com>
2023-03-07 00:48:54 +0530

    server deployed added end point

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-axios.defaults.baseURL = "http://localhost:5000";
+axios.defaults.baseURL = "https://movie-booking-app-backend.onrender.com";
 root.render(
   <React.StrictMode>
     <BrowserRouter>
